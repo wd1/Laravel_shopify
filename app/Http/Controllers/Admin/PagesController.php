6603492681a5ef169a32acc067f03cdd9c25c6bf
WD1Master <akmksi211@gmail.com>
2018-04-10 02:11:01 +0800

    Update 4.10

--- app/Http/Controllers/Admin/PagesController.php
+++ app/Http/Controllers/Admin/PagesController.php
@@ -10,33 +10,8 @@ class PagesController extends Controller
 
     public function getDashboard()
     {
-        $sh = \App::make('ShopifyAPI', ['API_KEY' => '1c06f823f80fe177c960e57f9424f04f', 'API_SECRET' => '954609206564ee424552f5849dc8c444', 'SHOP_DOMAIN' => 'nymbltest.myshopify.com', 'ACCESS_TOKEN' => '']);
-        $authorization_uri = $sh->installURL([
-            'permissions' => array('write_orders', 'write_products'),
-            'redirect' => 'https://nymbl.io/dashboard'  // update this to where Shopify should redirect the user
-        ]);
-        echo sprintf('<a href=\'%s\'>Shopify Login</a>', $authorization_uri);
-        // $sh = App::make('ShopifyAPI', ['API_KEY' => '', 'API_SECRET' => '', 'SHOP_DOMAIN' => '']);
-        // $sh = \App::make('ShopifyAPI', ['API_KEY' => '1c06f823f80fe177c960e57f9424f04f', 'API_SECRET' => '954609206564ee424552f5849dc8c444', 'SHOP_DOMAIN' => 'nymbltest.myshopify.com', 'ACCESS_TOKEN' => '']);
-        // try
-        // {
-        //     // $verify = $sh->verifyRequest(Input::all());
-        //     // if ($verify)
-        //     {
-        //         $code = 'accesscode';
-        //         $accessToken = $sh->getAccessToken($code);
-        //     }
-        //     // else
-        //     // {
-        //     //     // Issue with data
-        //     // }
-
-        // }
-        // catch (Exception $e)
-        // {
-        //     echo '<pre>Error: ' . $e->getMessage() . '</pre>';
-        // }
-        // return view('admin.pages.dashboard');
+        
+        return view('admin.pages.shopify_link');
     }
 	
 	public function getBlank()
---
+++ resources/views/admin/pages/shopify_link.blade.php
@@ -0,0 +1,30 @@
+<?php
+$sh = \App::make('ShopifyAPI', ['API_KEY' => '1c06f823f80fe177c960e57f9424f04f', 'API_SECRET' => '954609206564ee424552f5849dc8c444', 'SHOP_DOMAIN' => 'nymbltest.myshopify.com', 'ACCESS_TOKEN' => '']);
+$authorization_uri = $sh->installURL([
+	'permissions' => array('write_orders', 'write_products'),
+	'redirect' => 'https://nymbl.io/dashboard/admin/blank#'  // update this to where Shopify should redirect the user
+]);
+// echo sprintf('<a href=\'%s\'>Shopify Login</a>', $authorization_uri);
+header('Location: '.$authorization_uri);
+die();
+// $sh = \App::make('ShopifyAPI', ['API_KEY' => '1c06f823f80fe177c960e57f9424f04f', 'API_SECRET' => '954609206564ee424552f5849dc8c444', 'SHOP_DOMAIN' => 'nymbltest.myshopify.com', 'ACCESS_TOKEN' => '']);
+// try
+// {
+// 	// $verify = $sh->verifyRequest(Input::all());
+// 	// if ($verify)
+// 	{
+// 		$code = 'accesscode';
+// 		$accessToken = $sh->getAccessToken($code);
+// 		echo $accessToken;
+// 	}
+// 	// else
+// 	// {
+// 	//     // Issue with data
+// 	// }
+
+// }
+// catch (Exception $e)
+// {
+// 	echo '<pre>Error: ' . $e->getMessage() . '</pre>';
+// }
+?>
