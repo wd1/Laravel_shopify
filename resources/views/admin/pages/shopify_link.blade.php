4b998a6881aa0e1f322caa9deed5eac444472474
WD1Master <akmksi211@gmail.com>
2018-04-11 09:22:23 +0800

    Update 4.10

--- resources/views/admin/pages/shopify_link.blade.php
+++ resources/views/admin/pages/shopify_link.blade.php
@@ -1,5 +1,5 @@
 <?php
-$sh = \App::make('ShopifyAPI', ['API_KEY' => '1c06f823f80fe177c960e57f9424f04f', 'API_SECRET' => '954609206564ee424552f5849dc8c444', 'SHOP_DOMAIN' => 'nymbltest.myshopify.com', 'ACCESS_TOKEN' => '']);
+$sh = \App::make('ShopifyAPI', ['API_KEY' => '1c06f823f80fe177c960e57f9424f04f', 'API_SECRET' => '954609206564ee424552f5849dc8c444', 'SHOP_DOMAIN' => 'nymbltest2.myshopify.com', 'ACCESS_TOKEN' => '']);
 $authorization_uri = $sh->installURL([
 	'permissions' => array('write_orders', 'write_products'),
 	'redirect' => 'https://nymbl.io/dashboard/register_login'  // update this to where Shopify should redirect the user
