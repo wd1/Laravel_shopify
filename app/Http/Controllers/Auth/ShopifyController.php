aa8fcf4dc91a0d02ee677115782bf7e68f17193f
WD1Master <akmksi211@gmail.com>
2018-04-18 19:48:15 +0800

    Register Update

--- app/Http/Controllers/Auth/ShopifyController.php
+++ app/Http/Controllers/Auth/ShopifyController.php
@@ -96,7 +96,8 @@ class ShopifyController extends Controller
             $shop->owner = $shopinfo->shop->shop_owner;
             $shop->save();
 
-            return redirect('/');
+            // return redirect('/');
+            return redirect()->route('admin.dashboard');
         }
     }
 }
