1694e4d0ca79da76572fa5a80460f3808d16b63e
WD1Master <akmksi211@gmail.com>
2018-04-28 03:09:14 +0800

    Update registration and dashboard upload

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -612,7 +612,7 @@
 													</div>
 												</div>
 											</li>
-											<li class="m-menu__item  m-menu__item--submenu"  data-menu-submenu-toggle="click" data-redirect="true" aria-haspopup="true">
+											<!-- <li class="m-menu__item  m-menu__item--submenu"  data-menu-submenu-toggle="click" data-redirect="true" aria-haspopup="true">
 												
 												<a href="{{ route('auth0login') }}" class="m-menu__link m-menu__toggle">
 													<span class="m-menu__item-here"></span>
@@ -621,7 +621,7 @@
 														Link Account
 													</span>
 												</a>
-											</li>
+											</li> -->
 										</ul>
 									</div>
 									<!-- END: Horizontal Menu -->
@@ -1508,9 +1508,9 @@
 											</div>
 										</div>
 										<div class="form-group m-form__group row">
-											<label class="col-form-label col-lg-3 col-sm-12">File Type Validation</label>
-											<div class="col-lg-4 col-md-9 col-sm-12">
-												<div class="m-dropzone dropzone m-dropzone--success dz-clickable" action="inc/api/dropzone/upload.php" id="m-dropzone-three">
+											
+											<div class="col-lg-12 col-md-12 col-sm-12">
+												<div style="padding: 100px;" class="m-dropzone dropzone m-dropzone--success dz-clickable" action="inc/api/dropzone/upload.php" id="m-dropzone-three">
 													<div class="m-dropzone__msg dz-message needsclick">
 														<h3 class="m-dropzone__msg-title">Drop files here or click to upload.</h3>
 														<span class="m-dropzone__msg-desc">Only .png, .jpg, and .jpeg are allowed for upload</span>
